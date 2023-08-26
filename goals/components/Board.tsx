90cbd12ac3d3a02ffc81f9f88ad699f2ea4b6b73
lensonfernandes <lenson.fernand@gmail.com>
2023-08-26 16:41:33 +0530

    GOAL- Initialized Zustand

--- goals/components/Board.tsx
+++ goals/components/Board.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect} from "react";
+import {
+  DragDropContent,
+  DragDropContext,
+  Droppable,
+} from "react-beautiful-dnd";
 
 const Board = () => {
-  return (
-    <div>Board</div>
-  )
-}
 
-export default Board
+  useEffect(() => {
+    //getBoard
+  
+    return () => {
+      
+    }
+  }, [])
+  
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div>{/*All columns*/}</div>}
+      </Droppable>
+    </DragDropContext>
+    // <div>Board</div>
+  );
+};
+
+export default Board;
