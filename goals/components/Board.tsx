7e343d8cd356ffd860a31a532ec8d6a32b4c321f
lensonfernandes <lenson.fernand@gmail.com>
2023-08-27 17:59:18 +0530

    GOAL- Code to persist Column Drag

--- goals/components/Board.tsx
+++ goals/components/Board.tsx
@@ -11,9 +11,10 @@ import {
 import Column from "./Column";
 
 const Board = () => {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
@@ -21,16 +22,35 @@ const Board = () => {
     //console.log(getBoard());
   }, [getBoard]);
 
-  const handleOnDragEnd = (Result : DropResult) => {
-    
-  }
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    //oustide board
+    if (!destination) return;
+
+    //column drag
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+  };
 
   console.log(board);
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided) => (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto" {...provided.droppableProps} ref={provided.innerRef}>
+          <div
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
             {Array.from(board.columns.entries()).map(([id, column], index) => (
               <Column key={id} id={id} todos={column.todos} index={index} />
             ))}
