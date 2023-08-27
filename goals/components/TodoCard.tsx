3348acb9abda3097342fbaf917b319bcdee52ae0
lensonfernandes <lenson.fernand@gmail.com>
2023-08-27 17:02:20 +0530

    GOAL- Style changes for ToDo Card

--- goals/components/TodoCard.tsx
+++ goals/components/TodoCard.tsx
@@ -1,26 +1,46 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from 'react-beautiful-dnd';
+import { XCircleIcon } from "@heroicons/react/20/solid";
+import React from "react";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
 
 type Props = {
-    todo: Todo;
-    index: number;
-    id: TypedColumn;
-    innerRef: (element: HTMLElement | null) => void;
-    draggableProps: DraggableProvidedDraggableProps;
-    dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
-}
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
 
 const TodoCard = ({
-    todo, index, id, innerRef, draggableProps, dragHandleProps
+  todo,
+  index,
+  id,
+  innerRef,
+  draggableProps,
+  dragHandleProps,
 }: Props) => {
   return (
-    <div {...draggableProps} {...dragHandleProps} ref={innerRef}
-    className='bg-white rounded-md space-y-2 drop-shadow'>
-        <h1>Hello</h1>
-    </div>
-  )
-}
+    <div
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
+      className="bg-white rounded-md space-y-2 drop-shadow"
+    >
+      <div className="flex justify-between items-center p-5">
+        <p>{todo.title}</p>
+        <button className="text-red-500">
+            <XCircleIcon className="ml-5 h-8 w-8" />
+        </button>
+      </div>
 
-export default TodoCard
+      {/* Image to be added */}
+    </div>
+  );
+};
+
+export default TodoCard;
