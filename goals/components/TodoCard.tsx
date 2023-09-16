4e9748329229d4dcebcec4e388fc65acc2542d72
lensonfernandes <lenson.fernand@gmail.com>
2023-09-16 18:46:27 +0530

    GOAL- Updated style of Todo Card

--- goals/components/TodoCard.tsx
+++ goals/components/TodoCard.tsx
@@ -32,11 +32,11 @@ const TodoCard = ({
       {...draggableProps}
       {...dragHandleProps}
       ref={innerRef}
-      className="bg-white rounded-md space-y-2 drop-shadow"
+      className="bg-white rounded-md space-y-2 drop-shadow-md"
     >
       <div className="flex justify-between items-center p-5">
         <p>{todo.title}</p>
-        <button className="text-red-500">
+        <button className="text-red-400 hover:text-red-600">
             <XCircleIcon className="ml-5 h-8 w-8" onClick={()=> deleteTask(index, todo, id) } />
         </button>
       </div>
